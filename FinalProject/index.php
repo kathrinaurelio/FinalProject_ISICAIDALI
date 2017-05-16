2ab97593dd3a6bf314a7a60ab53dc69a9ea284d2
kathrinaurelio <kathrinaurelio@gmail.com>
2017-05-16 14:22:05 +0100

    Add files via upload
    Just a short test for pushing and pulling the code

---
+++ FinalProject/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>This could be the index-page of our project</title>
+    </head>
+    <body>
+        <?php
+        echo "Hello team!"
+        ?>
+    </body>
+</html>
