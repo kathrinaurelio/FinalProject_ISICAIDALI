6b1e41524be3af5981f5b54623bbbd6e29fab1d7
Kathrin\KathrinAurelio <kathrinaurelio@gmail.com>
2017-05-23 14:42:03 +0100

    This code should work if the database was named db

--- FinalProject/includes/config.php
+++ FinalProject/includes/config.php
@@ -3,14 +3,35 @@ ob_start();
 session_start();
 
 //database credentials
-define('DBHOST','localhost');
-define('DBUSER','database username');
-define('DBPASS','database password');
-define('DBNAME','database name');
+const DB_DSN = 'mysql:host=localhost;dbname=db';
+const DB_USER = 'root';
+const DB_PASS = '';
 
-$db = new PDO("mysql:host=".DBHOST.";port=8889;dbname=".DBNAME, DBUSER, DBPASS);
+$db = new PDO(DB_DSN, DB_USER, DB_PASS);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 //set timezone
 date_default_timezone_set('Europe/London');
 
+/*
+//load classes as needed
+function __autoload($class) {
+   
+   $class = strtolower($class);
+
+   $classpath = 'classes/class.'.$class . '.php';
+   if ( file_exists($classpath)) {
+      require_once $classpath;
+    }     
+
+   $classpath = '../classes/class.'.$class . '.php';
+   if ( file_exists($classpath)) {
+      require_once $classpath;
+    }
+        
+     
+}
+//pass the database connection
+$user = new User($db); 
+ * 
+ */
