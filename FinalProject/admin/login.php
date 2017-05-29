3a9aad305d0639662dfaca5cc106645c72163f99
Laura Griggs <laurajaynegriggs@gmail.com>
2017-05-29 16:05:58 +0100

    commented out the line requesting user be logged in, as they are on login page.

--- FinalProject/admin/login.php
+++ FinalProject/admin/login.php
@@ -3,7 +3,7 @@
 require_once('../includes/config.php');
 
 //if not logged in redirect to login page
-if(!$user->is_logged_in()){ header('Location: login.php'); }
+//if(!$user->is_logged_in()){ header('Location: login.php'); }
 ?>
 
 <form action="" method="post">
